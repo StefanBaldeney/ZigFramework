532a3434df6addfd64e2713c74e4b9ed6fec7bae
Stefan Brandt <stefan-brandt@live.de>
2024-04-08 01:13:09 +0200

    added SourceCode Darstellung

--- ZigIDE3/ViewModel/CodeViewModel.cs
+++ ZigIDE3/ViewModel/CodeViewModel.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using ZigIDE3.Properties;
+using ZigIDE3.Tool;
 
 namespace ZigIDE3.ViewModel
 {
@@ -15,14 +18,26 @@ namespace ZigIDE3.ViewModel
         {
             var path = Settings.Default.ZigPath;
 
+            SelectionChangedCommand = new RelayCommand(ExecuteLoadFileCommand);
+
             if (Directory.Exists(path))
             {
                 LoadFilesFromZigPathAsync();
             }
         }
 
+        private void ExecuteLoadFileCommand(object obj)
+        {
+            var zigPath = Settings.Default.ZigPath;
+
+            var filePath = Path.Combine(zigPath, obj as string);
+            this.SourceCode = loadZigFile(filePath);
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
+        public ICommand SelectionChangedCommand { get; }
+        
         private string _sourceCode;
         private string _output;
 
@@ -68,6 +83,21 @@ namespace ZigIDE3.ViewModel
                 }
 
                 return list;
+        }
+
+        private string loadZigFile(string filePath)
+        {
+            try
+            {
+                string fileContent = File.ReadAllText(filePath);
+                return fileContent;
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show($"Ein Fehler ist aufgetreten beim Lesen der Datei: {ex.Message}");
+            }
+
+            return null;
         }
 
         private IList<string> dateiListe= new List<string>();
