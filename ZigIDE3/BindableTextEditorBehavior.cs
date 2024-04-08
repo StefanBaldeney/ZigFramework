4d108f1595c44f2a58dd72a60a266782c0a41fe7
Stefan Brandt <stefan-brandt@live.de>
2024-04-08 13:10:35 +0200

    added Avalon mit bindable SourceCode

---
+++ ZigIDE3/BindableTextEditorBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using ICSharpCode.AvalonEdit;
+using Microsoft.Xaml.Behaviors;
+
+namespace ZigIDE3
+{
+    public class BindableTextEditorBehavior : Behavior<TextEditor>
+    {
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(BindableTextEditorBehavior),
+                new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, PropertyChangedCallback));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.TextChanged += AssociatedObjectOnTextChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.TextChanged -= AssociatedObjectOnTextChanged;
+        }
+
+        private void AssociatedObjectOnTextChanged(object sender, EventArgs eventArgs)
+        {
+            Text = AssociatedObject.Text;
+        }
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var behavior = dependencyObject as BindableTextEditorBehavior;
+            var editor = behavior?.AssociatedObject;
+            if (editor != null && editor.Text != behavior.Text)
+            {
+                editor.Text = behavior.Text;
+            }
+        }
+    }
+}
