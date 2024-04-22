1faf62f233b0bbb4a2366cbd1600695f86cb175d
Stefan Brandt <stefan-brandt@live.de>
2024-04-22 11:10:32 +0200

    added Binding 4 Snippets

---
+++ ZigIDE3/ViewModel/SnippetsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ZigIDE3.ViewModel
+{
+    public class SnippetsViewModel : INotifyPropertyChanged
+    {
+        private string _snippets;
+
+        public string Snippets
+        {
+            get => _snippets;
+            set
+            {
+                if (value == _snippets) return;
+                _snippets = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
